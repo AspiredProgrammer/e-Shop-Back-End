8701e43439692ead9eeec0553dc607597e79f14d
Bruno Fernandes da Cunha Santos <fernandesbruno3232@hotmail.com>
2024-04-15 16:20:04 -0400

    Changes on db_config

--- config/db_config.js
+++ config/db_config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  database:
-    "mongodb+srv://fernandesbruno3232:MauNjmm7LfZ9BBlI@cluster0.qiecgpz.mongodb.net/", 
-    //ask about whether this is needed since it's already in the .env file
-  secret: "secret",
+  database: process.env.MONGODB,
+  //ask about whether this is needed since it's already in the .env file
+  secret: process.env.SECRET,
 };
